0f47f867a6b4a59ebc225955e2d9be39a6928d20
˜David <davidtrabuco88@gmail.com>
2026-04-26 21:57:31 -0300

    Remove pasta duplicada barber-studios

--- barber-studios/src/hooks/useScroll.ts
+++
@@ -1,8 +0,0 @@
-export default function useScroll() {
-  function handleScroll(href: string) {
-    const target = document.querySelector<HTMLElement>(href)
-    target?.scrollIntoView({ behavior: 'smooth', block: 'center' })
-  }
-
-  return { handleScroll }
-}
